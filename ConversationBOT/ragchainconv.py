a58b242fb1b3c7371d36edd1ceea8f46c49ee82d
Vysix <Tymenvis@outlook.com>
2023-12-04 13:53:02 +0100

    added new prompt

--- ConversationBOT/ragchainconv.py
+++ ConversationBOT/ragchainconv.py
@@ -34,7 +34,7 @@ vectorstore = Chroma.from_documents(documents=texts, embedding=OpenAIEmbeddings(
 
 retriever = vectorstore.as_retriever()
 
-_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+_template = """Given the following conversation and a follow up question, in its original language.
 
 Chat History:
 {chat_history}
@@ -42,7 +42,7 @@ Follow Up Input: {question}
 Standalone question:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-template = """Answer the question based only on the following context:
+template = """Answer the question based only on the following context or Chat History:
 {context}
 
 Question: {question}
@@ -107,3 +107,4 @@ while True:
     input_text = input("Human: ")
     inputs = {"question": input_text}
     result = final_chain.invoke(inputs)
+    print(result)
